754579dd74f4168a376f9f412a7ec44a81530652
Angel Serrato <serrato.anggel@gmail.com>
2026-05-29 22:04:20 -0500

    fix(layout): allow global css import for app router

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
+// 1 CSS Styling
+// Add CSS rules to all the routes in your application, it is a good practice to add it to your top level component
 import "@/app/ui/global.css";
+// 3 Fonts and images
+// Add the font to the body element
 import { inter } from "@/app/ui/fonts";
 
 export default function RootLayout({
@@ -8,6 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* 3 Fonts and images Add the font here and you can also use antialiased which smooths out the font */}
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,5 @@
+// 3 Fonts and images
+// Using this file you can keep the fonts that you will be using in your app
 import { Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
